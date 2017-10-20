a35bcee2e319430ef80f188293f881767947d10f
liang <liyuliang2013@gmail.com>
2017-10-20 16:26:52 +0800

    init the dependence

---
+++ parser.go
@@ -0,0 +1,40 @@
+package parser
+
+import (
+	"bytes"
+	"github.com/PuerkitoBio/goquery"
+)
+
+type parser struct {
+	dom *goquery.Document
+}
+
+func InitDom(html string) (*parser, error) {
+
+	p := new(parser)
+
+	htmlBuf := bytes.NewBufferString(html)
+	dom, err := goquery.NewDocumentFromReader(htmlBuf)
+	if err != nil {
+
+		return p, err
+	}
+
+	p.dom = dom
+	return p, nil
+}
+
+func (query *parser) Find(selector string) *goquery.Selection {
+	return query.dom.Find(selector)
+}
+
+func (query *parser) FindAll(selector string) []*goquery.Selection {
+	var doms []*goquery.Selection
+	query.dom.Find(selector).Each(func(i int, s *goquery.Selection) {
+		doms = append(doms, s)
+	})
+	return doms
+}
+
+func (query *parser) Clear() {
+}
